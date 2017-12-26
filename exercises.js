46d4abd6ac9bee53a49e4c772e015b0484cad1e1
unknown <jzlau86@gmail.com>
2017-12-26 13:43:38 -0500

    Finished Exercises lesson 6

--- exercises.js
+++ exercises.js
@@ -2,115 +2,188 @@
 
 function multiplyByTen(num) {
   //return num after multiplying it by ten
+ return num * 10;
+  
 }
 
 function subtractFive(num) {
   //return num after subtracting five
+  return num - 5;
+  
 }
 
 function areSameLength(str1, str2) {
   //return true if the two strings have the same length
   //otherwise return false
+  if(str1.length === str2.length){
+    return true;
+  } else{
+    return false;
+  }
 }
 
 function areEqual(x, y) {
   //return true if x and y are the same
   //otherwise return false
+if( x === y){
+  return true;
+} else{
+  return false;
+}
+
 }
 
 function lessThanNinety(num) {
   //return true if num is less than ninety
   //otherwise return false
+  if(num < 90){
+    return true;
+  } else {
+    return false;
+  }
 }
 
 function greaterThanFifty(num) {
   //return true if num is greater than fifty
   //otherwise return false
-}
+  if (num > 50){
+    return true;
+  }
+      else {
+        return false;
+      }
+    }
+  
 
 function add(x, y) {
   //add x and y together and return the value
+  var sum = x + y;
+  return sum;
 }
 
 function subtract(x, y) {
   //subtract y from x and return the value
+  var sum = x-y;
+  return sum;
 }
 
 function divide(x, y) {
   //divide x by y and return the value
+  var div = x/y;
+  return div;
 }
 
 function multiply(x, y) {
   //multiply x by y and return the value
+  var mult = x * y;
+  return mult;
 }
 
 function getRemainder(x, y) {
   //return the remainder from dividing x by y
+  var mod = x % y;
+  return mod;
 }
 
 function isEven(num) {
   //return true if num is even
   //otherwise return false
+  if ( num%2 ===0){
+    return true;
+  } else {
+    return false;
+}
 }
 
 function isOdd(num) {
   //return true if num is odd
   //otherwise return false
+  if (num % 2 != 0){
+    return true;
+  } else{
+    return false;
+  }
 }
 
 function square(num) {
   //square num and return the new value
+  var square = (num*num);
+  return square;
 }
 
 function cube(num) {
   //cube num and return the new value
+  var cube = (num*num*num);
+  return cube;
 }
 
 function raiseToPower(num, exponent) {
   //raise num to whatever power is passed in as exponent
+ var a=  Math.pow(num,exponent);
+   return a;
 }
 
 function roundNumber(num) {
   //round num and return it
+  var round = Math.round(num);
+  return round;
 }
 
 function roundUp(num) {
   //round num up and return it
+  var up = Math.ceil(num);
+  return up;
 }
 
 function addExclamationPoint(str) {
   //add an exclamation point to the end of str and return the new string
   //'hello world' -> 'hello world!'
+  var ex = str.concat('!');
+  return ex;
 }
 
 function combineNames(firstName, lastName) {
   //return firstName and lastName combined as one string and separated by a space.
   //'Lambda', 'School' -> 'Lambda School'
+  var c = firstName + ' ' + lastName;
+  return c;
 }
 
 function getGreeting(name) {
   //Take the name string and concatenate other strings onto it so it takes the following form:
   //'Sam' -> 'Hello Sam!'
+  var hello= 'Hello ';
+  var g = hello.concat(name + '!');
+  return g;
 }
 
 //If you can't remember these area formulas then head over to Google or look at the test code.
 
 function getRectangleArea(length, width) {
   //return the area of the rectangle by using length and width
+  var a = length*width;
+  return a;
 }
 
 function getTriangleArea(base, height) {
   //return the area of the triangle by using base and height
+  var a = (base*height)/2;
+  return a;
 }
 
 function getCircleArea(radius) {
   //return the rounded area of the circle given the radius
+ var rad = (Math.PI) * (Math.pow(radius,2));
+  var radround = Math.round(rad);
+  return radround;
 }
 
 function getRectangularPrismVolume(length, width, height) {
   //return the area of the 3D rectangular prism given the length, width, and height
+//var a = (2 *(length*height))+ (2 * (length*height)) + (2*(length*width));
+var a = length * width* height;
+return a;
 }
-
 //Do not modify code below this line.
 ////--------------------------------
 
